74148e911dce4a43281341b6569017b6d19aac88
Ian Douglas Scott <ian@iandouglasscott.com>
2019-05-10 12:51:45 -0700

    Remove some commented/unused code

--- hello.c
+++ hello.c
@@ -6,21 +6,9 @@ long syscall(long, long, long, long, long, long, long);
 ssize_t write(int fd, const void *buf, size_t count) {
     // XXX remember errno
     return syscall(4, fd, (long)buf, count, 0, 0, 0);
-    /*
-      var SYS_WRITE = 4;
-      var fd = SYSCALLS.get(), buf = SYSCALLS.get(), count = SYSCALLS.get();
-      return SYSCALLS.browsix.syscall.sync(SYS_WRITE, fd, buf, count);
-      */
-}
-
-void exit(int status) {
-    //syscall(0, status);
 }
 
 int main() {
-    //syscall(1);
-    //syscall(1, 2, 3, 4, 5, 6, 7);
     write(1, "Hello world!\n", 13);
-    //exit(2);
     return 0;
 }
