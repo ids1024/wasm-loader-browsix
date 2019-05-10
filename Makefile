b6d4881521d18beea5a2543f1d1d5dbf6def189e
Ian Douglas Scott <ian@iandouglasscott.com>
2019-05-10 12:41:08 -0700

    Still not working?

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CFLAGS := -nostdlib -Wl,--no-entry -Wl,--export=main -Wl,--allow-undefined-file=functions -Wl,--import-memory --target=wasm32 -Os
+CFLAGS := -nostdlib -Wl,--no-entry -Wl,--export=main -Wl,--allow-undefined-file=functions -Wl,--import-memory -Wl,--shared-memory -Wl,--max-memory=67108864 --target=wasm32 -Os
 
 hello.wasm: hello.c
 	clang $(CFLAGS) $< -o $@
--- hello.js
+++ hello.js
@@ -6,7 +6,7 @@ else
 
 if (isNode) {
   var fs = require('fs');
-  var readFileAsync = fs.ReadFile;
+  var readFileSync = fs.ReadFileSync;
 
   /*
   var ffi = require('ffi');
@@ -19,9 +19,29 @@ if (isNode) {
   var argv = process.argv.slice(1);
   var argc = argv.length;
 } else {
-  function readFileAsync(path) {
-    fd = syscall(5, path, 0, 0, 0, 0, 0);
-    // TODO
+  function readFileSync(path) {
+    // XXX better error handling
+    var encoder = new TextEncoder();
+    var path_bytes = encoder.encode(path);
+    HEAPU8.set(path_bytes, 16); // XXX 16
+    HEAPU8[16 + path_bytes.length] = 0; // NUL
+    fd = syscall(5, 16, 0, 0, 0, 0, 0); // open
+    console.log(fd);
+    if (fd < 0) {
+      console.log("open() Failed: ", fd);
+    }
+    
+    addr = 32; // XXX
+    len = 0;
+    do {
+      bytes = syscall(3, fd, addr + len, 1024, 0, 0, 0); // read
+      len += bytes;
+    } while (bytes > 0);
+    if (bytes < 0) {
+      console.log("read() Failed: ", bytes);
+    }
+    console.log("Read ", len, "byte file")
+    return HEAPU8.slice(addr, addr+len);
   }
 }
 
@@ -34,6 +54,7 @@ memory = new WebAssembly.Memory({
   'shared': true
 });
 
+HEAPU8 = new Uint8Array(memory.buffer);
 HEAP32 = new Int32Array(memory.buffer);
 
 //console.log(typeof memory.buffer);
@@ -163,19 +184,16 @@ function init1(data) {
 
     console.log('B');
 
-    syscall(252, 42);
+    //syscall(252, 42);
 
-    console.log('C');
+    //console.log('C');
 
-    /*
     var importObject = {'env': env};
-    readFileAsync('hello.wasm').then(bytes =>
-      WebAssembly.instantiate(bytes, importObject)
-    ).then(results => {
+    var bytes = readFileSync('hello.wasm');
+    WebAssembly.instantiate(bytes, importObject).then(results => {
       // XXX envp
       results.instance.exports.main(argc, argv);
     });
-    */
   });
 }
 
