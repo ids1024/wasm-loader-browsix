9fb077a69a6b2d89a6f607dacd4897dc6b93b6dd
Ian Douglas Scott <ian@iandouglasscott.com>
2019-05-23 13:15:25 -0700

    Initialize auxv to 0 (musl accesses it by reading past end on environ)

--- wasm.ts
+++ wasm.ts
@@ -259,6 +259,12 @@ function write_args_environ_to_heap(args: string[], environ: string[]): [number,
   HEAP32[__heap_end / 4] = 0;
   __heap_end += 4;
 
+  // auxv
+  // http://articles.manugarg.com/aboutelfauxiliaryvectors
+  HEAP32[__heap_end / 4] = 0;
+  HEAP32([__heap_end+1) / 4] = 0;
+  __heap_end += 8;
+
   // Write arguments and populate argv
   for (var i = 0; i < args.length; i++) {
     HEAP32[argv / 4 + i] = __heap_end;
